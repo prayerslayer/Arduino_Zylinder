e91c133b21da131d46ec4297b9f293ff076913ce
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-01-29 16:30:33 +0100

    prozentual leds anschalten

--- ledSketch.ino
+++ ledSketch.ino
@@ -7,11 +7,12 @@ const int BLUE_PIN = 11;
 
 // CONSTANTS
 const int LED_COUNT = 2;
-const int DEFAULT_STEP_DELAY = 10;  // 10 ms
-const int DEFAULT_STEP = 5;         // 5 gray values
+const int DEFAULT_COLOR_DELAY = 10;  // 10 ms
+const int DEFAULT_PCNTG_DELAY = 50;   //50 ms
+const int DEFAULT_COLOR_STEP = 5;         // 5 gray values
+const int DEFAULT_PCNTG_STEP = 51;  // 20 % brightness
 
 // GLOBAL VARS
-boolean ledState[ LED_COUNT ];
 int ledIntensity[ LED_COUNT ];
 int ledPin[ LED_COUNT ];
 
@@ -23,31 +24,33 @@ int COLOR_BLUE[3] = { 0, 0, 255 };
 int currentColor[3] = { 0, 0, 0 };
 
 //Sets brightness of LED
-void setLedBrightness( int led, float intensity ) {
-  int rounded = floor( intensity * 255 );
-  ledIntensity[ led ] = rounded;
-  analogWrite( ledPin[ led ], rounded );
+void setLedBrightness( int led, int intensity ) {
+  ledIntensity[ led ] = intensity;
+  analogWrite( ledPin[ led ], intensity );
 }
 
 // Turns led group on or off
 void powerLed( int led, boolean on ) {
   if ( on ) {
-    setLedBrightness( led, 1.0 );
+    setLedBrightness( led, 255 );
   } else {
-    setLedBrightness( led, 0.0 );
+    setLedBrightness( led, 0 );
   }
 }
 
+// Sets green value of displayed color
 void setColorGreen( int green ) {
   currentColor[ 1 ] = green;
   analogWrite( GREEN_PIN, 255 - green );
 }
 
+// Sets red value of displayed color
 void setColorRed( int red ) {
   currentColor[ 0 ] = red;
   analogWrite( RED_PIN, 255 - red );
 }
 
+// Sets blue value of displayed color
 void setColorBlue( int blue ) {
   currentColor[ 2 ] = blue;
   analogWrite( BLUE_PIN, 255 - blue );
@@ -80,10 +83,42 @@ void setup()  {
   powerLed( 1, HIGH );
 }
 
+// Fades a LED brightness to target value
+void fadeLed( int led, int targetval ) {
+  int currentval = ledIntensity[ led ];
+  int stepp = currentval < targetval ? DEFAULT_PCNTG_STEP : DEFAULT_PCNTG_STEP * (-1);
+  while ( currentval != targetval ) { // + 1 because 255/32 is no integer
+    currentval += stepp;
+    setLedBrightness( led, currentval );
+    delay( DEFAULT_PCNTG_DELAY );
+  }
+}
+
+int getActiveLeds() {
+  int activeLeds = 0;
+  for ( int i = LED_COUNT - 1; i >= 0; i-- ) {
+    activeLeds += ledIntensity[ i ] > 0 ? 1 : 0;
+  }
+  return activeLeds;
+}
+
+void fadeToPercentage( float percentage ) {
+  int necessaryLeds = round( percentage * LED_COUNT );
+  int activeLeds = getActiveLeds();
+  if ( activeLeds == necessaryLeds )
+    return;
+  int increment = necessaryLeds > activeLeds ? 1 : -1;
+  int target = increment > 0 ? 255 : 0;
+  int indexmodifier = increment > 0 ? 0 : -1;
+  do {
+    fadeLed( activeLeds + indexmodifier, target );
+    activeLeds += increment;
+  } while ( necessaryLeds != activeLeds );
+}
 
 // Fades LEDs to given color
 void fadeToColor( int color[ 3 ] ) {
-   fadeToColor( color, DEFAULT_STEP_DELAY );
+   fadeToColor( color, DEFAULT_COLOR_DELAY );
 }
 
 // Fades LEDs to given color
@@ -92,9 +127,9 @@ void fadeToColor( int color[ 3 ], int stepdelay ) {
   int diffGreen = color[ 1 ] - currentColor[ 1 ]; //possibly negative!
   int diffBlue = color[ 2 ] - currentColor[ 2 ];  //possibly negative!
   
-  int stepRed = diffRed < 0 ? DEFAULT_STEP * (-1) : DEFAULT_STEP;
-  int stepGreen = diffGreen < 0 ? DEFAULT_STEP * (-1) : DEFAULT_STEP;
-  int stepBlue = diffBlue < 0 ? DEFAULT_STEP * (-1) : DEFAULT_STEP;
+  int stepRed = diffRed < 0 ? DEFAULT_COLOR_STEP * (-1) : DEFAULT_COLOR_STEP;
+  int stepGreen = diffGreen < 0 ? DEFAULT_COLOR_STEP * (-1) : DEFAULT_COLOR_STEP;
+  int stepBlue = diffBlue < 0 ? DEFAULT_COLOR_STEP * (-1) : DEFAULT_COLOR_STEP;
 
   int tempRed = 0;
   int tempGreen = 0;
@@ -109,7 +144,7 @@ void fadeToColor( int color[ 3 ], int stepdelay ) {
     if ( fadeRed ) {
       tempRed = currentColor[ 0 ] + stepRed;
       setColorRed( tempRed );
-      fadeRed = diffRed < 0 ? tempRed > color[ 0 ] : tempRed < color[ 0 ]; // tempRed != target value
+      fadeRed = diffRed < 0 ? tempRed > color[ 0 ] : tempRed < color[ 0 ]; // means basically tempRed != target value
     }
 
     // fade green
@@ -199,4 +234,3 @@ void loop() {
   fadeToPercentage( getVoltage() / 5 );
   fadeToColor( color );
 }
-
